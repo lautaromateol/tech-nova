38a48ad6d2040599b787cf06f7241de8c1bedbb9
lautaromateol <lautaromateol@gmail.com>
2025-03-18 08:44:32 -0300

    update: useUser response type name

--- features/auth/hooks/use-user.ts
+++ features/auth/hooks/use-user.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import { API_URL } from "@/constants/api-url";
-import { User } from "@/features/user/types";
+import { UserDto } from "@/features/user/dtos/user-dto";
 
 export function useUser({ id, token }: { id: string, token: string }) {
   const { data: user, isLoading: isLoadingUser } = useQuery({
     queryKey: ["user", { id }],
     queryFn: async () => {
       try {
-        const response = await axios.get<User>(`${API_URL}/users/${id}`,
+        const response = await axios.get<UserDto>(`${API_URL}/users/${id}`,
           {
             headers: {
               Authorization: `Bearer ${token}`
